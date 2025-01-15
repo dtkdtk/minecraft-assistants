d2cfa6584f746ac740c02e6e097107224a271829
dtkdtk <dtkdtk0.mail@gmail.com>
2025-01-15 22:01:52 +0300

    Example of TS migration

--- src/modules/core/chat-commands.ts
+++ src/modules/core/chat-commands.ts
@@ -2,17 +2,11 @@ import * as mf from "mineflayer";
 
 export default class Mod_ChatCommands {
 
-  /**
-   * @param {Bot} bot 
-   */
-  constructor(bot) {
+  constructor(bot: Bot) {
     bot.on("chat", (...args) => this.handleMessage(...args));
   }
 
-  /**
-   * @type {BotEvents["chat"]}
-   */
-  async handleMessage(username, message, translate, jsonMsg, matches) {
+  handleMessage: BotEvents["chat"] = async function(username, message, translate, jsonMsg, matches) {
     console.log(`Получил сообщение: <${username}> ${message}`);
   }
 }
