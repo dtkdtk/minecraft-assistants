edbcf35918d917ea0fee50a0bf2d61bf0d534e17
Valinok37 <160887574+Valinok37@users.noreply.github.com>
2025-01-18 22:21:14 +0300

    Update init-modules.ts

--- src/init-modules.ts
+++ src/init-modules.ts
@@ -1,11 +1,8 @@
 import Mod_ChatCommands from "./modules/core/chat-commands.js";
-import Mod_Farmer from "./modules/core/farmer.js";
-
 /**
  * Инициализирует все необходимые модули бота.
  * Вызывается в главном файле.
  */
 export default function initModules(bot: Bot) {
   new Mod_ChatCommands(bot);
-  new Mod_Farmer(bot);
 }
