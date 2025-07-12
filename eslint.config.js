bb267acfa22f50ed069d43c1e8dcfa0b8e2bd63c
dtkdtk <dtkdtk0.mail@gmail.com>
2025-07-12 14:55:42 +0300

    ...

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,13 @@ import js from "@eslint/js";
 import globals from "globals";
 import tsEslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
+  includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
+  { files: ["{lib,exe}/**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["{lib,exe}/**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
   tsEslint.configs.recommended,
 ]);
