b94e9ff79f60ee87dd12150895846f9af38b0512
Valinok37 <160887574+Valinok37@users.noreply.github.com>
2025-08-12 16:16:21 +0300

    rename (WIP)
    Co-Authored-By: dtkdtk0 <93874976+dtkdtk@users.noreply.github.com>

--- exe/subthread.js
+++ exe/subthread.js
@@ -1,2 +1,2 @@
-import { createMinecraftAssistantBot } from "../dist/lib/init_bot.js";
+import { createMinecraftAssistantBot } from "../dist/core/init_bot.js";
 createMinecraftAssistantBot(JSON.parse(process.env.BOT_CONFIGURATION));
