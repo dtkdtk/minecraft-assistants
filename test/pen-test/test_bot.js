6acc4214022fd248c8da21495fbc6b6c066d77c6
dtkdtk <dtkdtk0.mail@gmail.com>
2025-10-08 21:00:45 +0300

    tests: fix pen-test

--- test/pen-test/test_bot.js
+++ test/pen-test/test_bot.js
@@ -1,9 +1,13 @@
 import { createMinecraftAssistantBot } from "../../dist/core/init_bot.js";
 import testBotCfg from "./test_bot_cfg.js";
+import { sep as pathSep } from "path";
+
+if (!process.cwd().endsWith("pen-test") && !process.cwd().endsWith("pen-test" + pathSep))
+  throw new Error("pen-test must be launched from 'test/pen-test/' directory");
 
 const config = {
   ...testBotCfg,
-  skillsDirPath: "../dist/skills/",
+  skillsDirPath: "../../dist/skills/",
 };
 
 createMinecraftAssistantBot(config);
